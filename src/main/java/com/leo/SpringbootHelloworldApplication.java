05f508e3ed6703efc1284a05b5dd167ab3b0620f
leo <sickenbabay@163.com>
2020-01-09 14:28:40 +0800

    4

--- src/main/java/com/leo/SpringbootHelloworldApplication.java
+++ src/main/java/com/leo/SpringbootHelloworldApplication.java
@@ -2,9 +2,16 @@ package com.leo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootHelloworldApplication {
+public class SpringbootHelloworldApplication extends SpringBootServletInitializer {
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringbootHelloworldApplication.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootHelloworldApplication.class, args);
